57b6bdfcf72c405546b64c6e6befbe36bddb7167
michaelvbend <brenzodj@gmail.com>
2024-12-12 18:25:28 +0100

    D: First custom component

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,23 +1,10 @@
-import React from 'react';
 import './App.css';
-
-// Define the App component
+import FirstComponent from './FirstComponent';
 function App() {
-  // Return the JSX structure using React.createElement
-  return React.createElement(
-    React.Fragment, // Use React.Fragment to group elements without adding extra nodes to the DOM
-    null, // No props for the fragment
-    React.createElement('h1', null, 'Hello World'), // Create an h1 element with the text "Hello World"
-    React.createElement('p', null, 'This is a paragraph.'), // Create a p element with the text "This is a paragraph."
-    React.createElement(
-      'div',
-      null,
-      React.createElement(
-        'button',
-        { onClick: () => alert('Button clicked!') }, // Add an onClick prop to the button
-        'Click me'
-      )
-    )
+  return (
+    <>
+      <FirstComponent />
+    </>
   );
 }
 
---
+++ my-react-app/src/FirstComponent.jsx
@@ -0,0 +1,12 @@
+function FirstComponent() {
+  return (
+    <>
+      <h1>First rule</h1>
+      <p>Function name starts with uppercase character</p>
+      <h2>Second rule</h2>
+      <p>Returns single renderable content</p>
+    </>
+  );
+}
+
+export default FirstComponent;
